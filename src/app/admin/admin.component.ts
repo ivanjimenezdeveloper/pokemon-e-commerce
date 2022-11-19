b25a248ea0634e10ea2b838221dd6b9452bebf60
Ivan Jimenez <ivanjimluqmallorca@gmail.com>
2022-11-19 14:20:04 +0100

    basic navbar

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
   constructor() {}
+  showMenuMobile: boolean = false;
 
   ngOnInit() {}
+
+  toggleMenu() {
+    this.showMenuMobile = !this.showMenuMobile;
+  }
 }
--- src/app/shared-modules/shared-modules.module.ts
+++ src/app/shared-modules/shared-modules.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { MatButtonModule } from '@angular/material/button';
 
-const MODULES_EXPORT = [MatButtonModule];
+const MODULES_EXPORT = [MatButtonModule, MatToolbarModule, MatIconModule];
 
 @NgModule({
   declarations: [],
